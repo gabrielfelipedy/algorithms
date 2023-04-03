7d3d0f069b5ebd25e4d936e7224250a13a435563
mathsstack <gabrielfelipeassuncaodesouza@gmail.com>
2023-04-03 13:51:51 -0300

    first commit

---
+++ java/bubble_sort.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class bubble_sort
+{
+	public static void main(String[] args)
+	{
+		int[] array = new int[5];
+		int n, i, aux;
+
+		//Inserting data into the array
+
+		Scanner s = new Scanner(System.in);
+
+		for(i = 0, i < 5; i++)
+		{
+			System.out.println("Digite o "+(i+1)+"° número");
+			array[i] = s.nextInt();
+		}
+
+		//Sorting
+		
+		for(n = 1; n <= 5; n++)
+		{
+			for(i = 0; i < 4; i++)
+			{
+				if(array[i] > array[i+1])
+				{
+					aux = array[i];
+					array[i] = array[i+1];
+					array[i+1] = aux;
+				}
+			}
+		}
+
+		//printing the array ordenated
+		for(i = 0; i < 5; i++)
+		{
+			System.out.println((i+1)+"° Element: "+ array[i])
+		}
+	}	
+}

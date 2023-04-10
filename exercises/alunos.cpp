5a71d91730f7c9b3c3ab6a264d3a40189fe6a959
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-04-10 10:58:12 -0300

    add last feature

--- exercises/alunos.cpp
+++ exercises/alunos.cpp
@@ -15,9 +15,53 @@ void printAluno(std::string aluno[]) {
 	double media = calculaMedia(aluno[NOTA1], aluno[NOTA2]);
 
 	std::cout << "\nNome: " << aluno[NOME] << std::endl;
-	std::cout << "Nota 2: " << aluno[NOTA1] << std::endl;
-	std::cout << "Nota 1: " << aluno[NOTA2] << std::endl;
+	std::cout << "Nota 1: " << aluno[NOTA1] << std::endl;
+	std::cout << "Nota 2: " << aluno[NOTA2] << std::endl;
 	std::cout << "Media: " << media << std::endl;
+}
+
+void printReprovados(std::string alunos[3][3])
+{
+	int i, j;
+	for(i=0;i<3;i++) {
+                for(j=0;j<2;j++) {
+
+                        int index = 0;
+                        while(true) {
+
+                        int s1 = tolower(alunos[j][NOME].at(index));
+                        int s2 = tolower(alunos[j+1][NOME].at(index));
+
+                        if(s1 > s2) {
+
+                                std::string aux[3];
+				std::copy(alunos[j], alunos[j]+3, aux);
+
+				std::copy(alunos[j+1], alunos[j+1]+3, alunos[j]);
+				std::copy(aux, aux+3, alunos[j+1]);
+
+                                break;
+
+                        } else if(s1 == s2) {
+
+                                index++;
+                                continue;
+                        } else
+                                break;
+                        }
+                }
+        }	
+
+	std::cout << "\n===REPROVED===\n";
+	j = 0;
+	while(j < 3)
+	{
+		if(calculaMedia(alunos[j][NOTA1], alunos[j][NOTA2])< 7)
+		{
+			printAluno(alunos[j]);
+		}
+		j++;
+	}
 }
 
 int main() {
@@ -51,11 +95,35 @@ int main() {
 					}
 		}
 	}
-	
+
+
 	//print
-	std::cout << "\n===CLASS INFO===\n";
+	std::cout << "\n===MEDIA ORDENATION===\n";
 	for(i=0; i < QTDE; i++)
 		printAluno(alunos[i]);
+
+	//sort
+	for(i=0; i < QTDE; i++) {
+
+		for(j=0; j < QTDE - 1; j++) {
+
+			if(stoi(alunos[j][NOTA1]) > stoi(alunos[j+1][NOTA1])) {
+
+				std::string aux[3];
+				std::copy(alunos[j], alunos[j] + 3, aux);
+				std::copy(alunos[j+1], alunos[j+1]+3, alunos[j]);
+				std::copy(aux, aux+3, alunos[j+1]);
+					}
+		}
+	}
+
+	//print
+	std::cout << "\n===1st NOTE ORDENATION===\n";
+	for(i=0; i < QTDE; i++)
+		printAluno(alunos[i]);
+
+	//print reproved
+	printReprovados(alunos);
 
 	return 0;
 }
